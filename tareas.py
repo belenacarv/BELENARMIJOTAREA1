323f23671d2009fe5bd9c6bd9119ed4769b2c9e2
Belen Armijo <69484595+belenacarv@users.noreply.github.com>
2022-08-24 14:30:08 -0400

    Update tareas.py

--- tareas.py
+++ tareas.py
@@ -1,3 +1,4 @@
+#se crea una funcion while para poder crear el loop, utilizando la funcion break en caso de escribir "salir"
 
 while(True):
     esc = input('ingrese dato, escriba salir si quiere finalizar: ')
