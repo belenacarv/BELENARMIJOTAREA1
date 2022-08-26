2f24950811f322c86999f8c5f1ce64751b48122d
Belen Armijo <69484595+belenacarv@users.noreply.github.com>
2022-08-26 19:25:20 -0400

    Delete tareas.py

--- tareas.py
+++
@@ -1,7 +0,0 @@
-# se crea una funcion while para poder crear el loop
-while(True):
-    esc = input('ingrese dato, escriba salir si quiere finalizar: ')  
-    # si la entrada es "salir" se usa el comando break para salir del loop
-    if(esc=='salir'):
-        break
-print('Fin')
