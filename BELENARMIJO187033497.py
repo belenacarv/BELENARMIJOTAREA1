988ce8ae6397f8b88ec93751739ea3a9a862022b
Belen Armijo <69484595+belenacarv@users.noreply.github.com>
2022-08-26 19:36:50 -0400

    Delete BELENARMIJO187033497.py

--- BELENARMIJO187033497.py
+++
@@ -1,6 +0,0 @@
-# se crea una funcion while para poder crear el loop
-while(True):
-    esc = input('ingrese dato, escriba salir si quiere finalizar: ')  
-    # si la entrada es "salir" se usa el comando break para salir del loop, y el código termina
-    if(esc=='salir'):
-        break
